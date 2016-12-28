4347633fe4a252b70086bf08a17141b23698c3b8
Sebastian Hellsing <sebastsing@gmail.com>
2016-12-28 17:05:34 -0500

    Stocks ticking
    Stock ticker now refreshes every 5 seconds. Working on color coding for
    ups and downs.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,10 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Main extends Application {
@@ -31,25 +25,8 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 		SampleController samp = new SampleController();
-		System.out.print(samp.getPrice("amrs"));
+		
+		samp.refresher();
+		
 	}
-}
-//Stock class that will hold a List of StockSymbols as well as passing data to the ListView
-class Stocks{
-    private List<StockSymbols> stockList;
-    
-    public Stocks() {
-	ListView<String> lists = new ListView<>();
-	ObservableList<String> items = FXCollections.observableArrayList();
-	lists.setItems(items);
-    }
-
-
-    public List<StockSymbols> newSymbol(String symbol){
-	this.stockList = new ArrayList<StockSymbols>();
-	this.stockList.add(new StockSymbols(symbol));
-	return this.stockList;
-    }
-    
-    
 }
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,6 +11,7 @@ import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.jsoup.Jsoup;
@@ -24,16 +26,21 @@ public class SampleController {
     @FXML
     private ObservableList<String> items = FXCollections.observableArrayList();
     @FXML
-    private ListView<String> lists = new ListView<>(items);
-    
-    private List<String> looper;
+    ListView<String> lists = new ListView<>(items);
+
+    private List<String> looper = new ArrayList<>();;
     private List<String> loopier = new ArrayList<>();
-    
-    
+    private HashMap<String, String> sMap = new HashMap<>();
+
+    //This method should take the symbol entered by the user and return
+    //stock price from yahoo financials. Contains the code to push the data
+    //to the list.
     public String getPrice(String sym){
 	try {
-	    if(!loopier.contains(sym)){
-		loopier.add(sym);
+	    if(loopier.contains(sym)){
+	    }
+	    else{
+	    loopier.add(sym);
 	    }
 	    Document doc = Jsoup.connect("http://finance.yahoo.com/webservice/v1/symbols/" + sym + "/quote").userAgent("Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>").get();
 	    StringBuilder sb = new StringBuilder(doc.toString());
@@ -41,65 +48,127 @@ public class SampleController {
 	    Elements element = doc.select("field[name=\"price\"");
 	    String elements = element.toString().replaceAll("<field name=\"price\">", "");
 	    elements = elements.replaceAll("</field>", "");
-	    String finalPrice = sym.toUpperCase().replace("\n", "") +": "+elements.replace("\n", "");
+	    String element2 = elements.replace("\n", "");
+	    if(!sMap.containsKey(sym.toUpperCase().replace("\n", ""))){
+		sMap.put(sym.toUpperCase().replace("\n", "") +": ", elements.replace("\n", ""));
+	    }
+	    else{
+		sMap.replace(sym.toUpperCase().replace("\n", ""), elements.replace("\n", ""));
+	    }
+	    sMap.put(sym.toUpperCase().replace("\n", "") +": " , element2);
+	    String finalPrice = sym.toUpperCase().replace("\n", "") + elements.replace("\n", "");
+	    System.out.print(sMap.keySet() + elements);
 	    return finalPrice;
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
 	return null;
     }
-    @FXML
-    public void initialize(){
-	this.addSymbol.setDisable(true);
-	this.lists.setItems(items);
-	StockLoop loopy = new StockLoop();
-	loopy.run();
+    @FXML //This method runs automatically once the app launches
+    //starts the loop to start the list refresh
+    public void initialize() throws InterruptedException{
+	addSymbol.setDisable(true);
+	lists.setItems(items);
+	Thread thread = new Thread(new Runnable() {
+		public void run(){
+		    refresher();
+		}
+		});
+	thread.start();
+	//StockLoop loopy = new StockLoop(looper, loopier, lists, items);
 	
     }
     
-    
-    
-    
-    @FXML
+    public void refresher(){	    			
+		    while(true){
+			Platform.runLater(() ->{
+			    this.items.clear();
+			});
+			    for(String s: this.loopier){
+				//if(this.loopier.contains(s)){
+				    //continue;
+				//}
+				Platform.runLater(() ->{
+				this.items.add(getPrice(s));
+				});
+
+			    }
+			    Platform.runLater(() ->{
+			    this.lists.setItems(items);
+			    
+			    });
+			    try {
+				Thread.sleep(5000);
+			    } catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			    }
+			    
+		    
+		}
+    }
+    @FXML //button to add stock symbol in the textbox to the list
     public void onButtonClick(){
-	this.items.add(getPrice(textBox.getText()));
+	items.add(getPrice(textBox.getText()));
 	textBox.clear();
 	lists.setItems(items);
+
     }
-    @FXML
+    @FXML 
+    //Checks to make sure there is text to add to the list, button will be disabled otherwise.
+    //TODO add more safety checks so that user cannot screw themselves.
     public void handleKeyReleased(){
 	String text = textBox.getText();
 	boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
-	addSymbol.setDisable(disableButtons);	
+	addSymbol.setDisable(disableButtons);
+	
     }
-    @FXML
+    @FXML //Allows stocks to be added when enter is pressed on the textbox.
     public void onEnter(ActionEvent e){
 	onButtonClick();
     }
 }
-class StockLoop extends SampleController implements Runnable{
-    List<String> looper;
-    List<String> loopier;
-    ListView<String> lists;
-    ObservableList<String> items;
-    
-    public void  run(){
-	try{ while(true){
-	    Thread.sleep(1000);
-	    items.clear();
-	    looper.clear();
-	    lists.setItems(items);
-	    for(String s: loopier){
-		if(loopier.contains(s)){
-		    continue;
-		}
-		items.add(getPrice(s));
-		
-	    }
-	}
-	} catch(Exception e){
-	    System.err.print(e);
-	}
-	
-    }
-}
+
+//Class to create another thread to refresh the stock list.
+//class StockLoop extends SampleController implements Runnable{
+//    List<String> looper;
+//    List<String> loopier;
+//    ListView<String> lists;
+//    ObservableList<String> items;
+
+
+//    public StockLoop(List<String> looper, List<String> loopier, ListView<String> lists, ObservableList<String> items) {
+//	this.looper = looper;
+//	this.loopier = loopier;
+//	this.lists = lists;
+//	this.items = items;
+//    
+//
+//
+//	public void run(){
+//	    
+//	Platform.runLater(() -> {
+//	    public void  run(){
+//	try{ while(true){
+//	    Thread.sleep(1000);
+//	    items.clear();
+//	    looper.clear();
+//	    lists.setItems(items);
+//	    for(String s: loopier){
+//		if(loopier.contains(s)){
+//		    continue;
+//		}
+//		items.add(getPrice(s));
+//
+//	    }
+//	    
+//	    lists.setItems(items);
+//	}
+//	} catch(Exception e){
+//	    System.err.print(e); 
+//	    }
+//	    }});
+//
+//    }
+//    
+//}
